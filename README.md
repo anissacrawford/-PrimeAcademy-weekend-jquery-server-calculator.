44adecb8dce37a5f4dd6f9c0cca6329074dc60ac
Anissa Crawford <anissacrawford@gmail.com>
2022-03-12 17:31:50 -0600

    added calulation functions and updated readme

--- README.md
+++ README.md
@@ -4,6 +4,24 @@
 
 ## Description
 
-Your project description goes here. What problem did you solve? How did you solve it?
+Duration: 3 days
 
-Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
+Users will be able to input two numbers and select the type of mathematical operation (addition, subtraction, multiplication, and division) and click the equal button to get their desired results. Users are also able to click the clear button to clear the input fields. 
+
+## Usage
+1. Add a number to the "number one" input field 
+2. Choose your desired mathematical operation (button)
+3. Add another number to the "number two" input field 
+4. Click the equal button to run your equation
+5. Click the clear button to clear your inputs 
+6. See previous calculations below
+
+## Built With 
+Visual Code Text Editor 
+
+## Acknowledgment 
+Thanks to Prime Digital Academy who equipped and helped me to make this application a reality. 
+
+## Support 
+If you have suggestions or issues, please email me at anissacrawford@gmail.com
+
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,7 +3,12 @@ $(document).ready(handleReady);
 function handleReady(){
     // console.log('hi')
     getCalculation();
-    $('#addBtn').on('click', handleAdd)
+    $('#addBtn').on('click', handleAdd);
+    $('#minusBtn').on('click', handleSubtract);
+    $('#multiplyBtn').on('click', handleMultiply);
+    $('#divideBtn').on('click', handleDivide);
+    $('#equalBtn').on('click', handleEqual);
+    $('#clearBtn').on('click', handleClear)
 };
 
 let calculation = {
@@ -19,21 +24,58 @@ function getCalculation (){
         method: 'GET'
     }).then(function(response){
         console.log(response);
-        // render(response);
+        render(response);
     }).catch(function(error){
         console.log(error);
         alert('error in get!')
     })
 }
 
-//function for add 
+//sum global variable
+let sum = 0
+
+//function for addition
 function handleAdd(){
-    let sum = calculation.numberOne + calculation.numberTwo;
+    sum = calculation.numberOne + calculation.numberTwo;
+    console.log('handleAdd');
+}
+
+//function for subtraction
+function handleSubtract(){
+    sum = calculation.numberOne - calculation.numberTwo;
+    console.log('handleSubtract');
+}
+
+//function for multiplication 
+function handleMultiply(){
+    sum = calculation.numberOne * calculation.numberTwo;
+    console.log('handleMultiply');
+}
+
+
+//function for division 
+function handleDivide(){
+    sum = calculation.numberOne / calculation.numberTwo;
+    console.log('handleDivide');
+
+}
+
+//function for equal
+function handleEqual(){
+    console.log('clicked!');
     return sum;
 }
 
-//function for minus
+function handleClear(){
+    $('numberOne').val('');
+    $('numberTwo').val('');
+}
 
-//function for multiply 
-
-//function for division 
+//handles DOM 
+function render(calculation){
+    for (const answers of calculation){
+        $('body').append(`<h3>
+        ${answers.numOne} ${answers.symbol} ${answers.numTwo}
+        </h3>`)
+    }
+}
